aa43fb5ed5e3dd1df6f3f43306e5dc1723dcef23
RioSakao <skoqush@gmail.com>
2023-09-20 11:25:08 -0700

    Resolved #5

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
+using namespace std;
 
 const int RGB_HEX_LENGTH = 7;
-
-int main(){
+int i = 0;
+int main()
+{
     std::string input;
 
-    do{
-        std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+    do
+    {
+        cout << "Enter a color in hex format (#RRGGBB):";
+        getline(cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+        if (input.size() != RGB_HEX_LENGTH)
+        {
+            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+	else
+	{
+        	if (input[0] != '#' && i==0 )
+        	{
+            		cout << "The first character must be #" << endl;
+        	}
+        	else (isdigit(!input[i] || isalpha(!input[i])))
+        	{
+            		cout << "Invalid input" << endl;
+        	}
+       		i++;
+	}
+    } while (input.size() != RGB_HEX_LENGTH);
 
-    std::cout << "Your hex color is: " << input << std::endl;
+    cout << "Your hex color is: " << input << endl;
 
     return 0;
 }
